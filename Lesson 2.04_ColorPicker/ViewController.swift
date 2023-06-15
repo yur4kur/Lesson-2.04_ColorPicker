424ec139060fb421fb1cad9aeb3a340d856a1560
Iurii Kurinnoi <yur4kur@icloud.com>
2023-06-15 10:58:00 +0400

    Sliders setup and connection to labels completed.

--- Lesson 2.04_ColorPicker/ViewController.swift
+++ Lesson 2.04_ColorPicker/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: IBOutlets
+    @IBOutlet var finalColorView: UIView!
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        connect(slider: redSlider, toLabel: redValueLabel, trackColor: .red)
+        connect(slider: greenSlider, toLabel: greenValueLabel, trackColor: .green)
+        connect(slider: blueSlider, toLabel: blueValueLabel, trackColor: .blue)
     }
-
+    
+    // MARK: IBActions
+    @IBAction func moveSlider(_ sender: UISlider) {
+        switch sender.tag {
+        case 0:
+            redValueLabel.text = String(format: "%.2f", sender.value)
+        case 1:
+            greenValueLabel.text = String(format: "%.2f", sender.value)
+        case 2:
+            blueValueLabel.text = String(format: "%.2f", sender.value)
+        default:
+            print("Something gone wrong")
+        }
+    }
+    
+    // MARK: Private methods
+    private func connect(slider: UISlider, toLabel label: UILabel, trackColor color: UIColor) {
+        label.text = slider.value.formatted()
+        slider.minimumTrackTintColor = color
+    }
+    
 
 }
 
