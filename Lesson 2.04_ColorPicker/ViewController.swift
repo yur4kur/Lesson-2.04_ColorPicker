bd06ee7441c704d73b6ce477684da3174dc0fa77
Iurii Kurinnoi <yur4kur@icloud.com>
2023-06-15 13:06:38 +0400

    View set up and color change methods completed.

--- Lesson 2.04_ColorPicker/ViewController.swift
+++ Lesson 2.04_ColorPicker/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
     
     //MARK: IBOutlets
     @IBOutlet var finalColorView: UIView!
-    @IBOutlet var mainView: UIView!
     
     @IBOutlet var redValueLabel: UILabel!
     @IBOutlet var greenValueLabel: UILabel!
@@ -21,33 +20,66 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    // MARK: Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        connect(slider: redSlider, toLabel: redValueLabel, trackColor: .red)
-        connect(slider: greenSlider, toLabel: greenValueLabel, trackColor: .green)
-        connect(slider: blueSlider, toLabel: blueValueLabel, trackColor: .blue)
+        setupView(finalColorView)
+        
+        setLabelText(of: redValueLabel, from: redSlider)
+        setLabelText(of: greenValueLabel, from: greenSlider)
+        setLabelText(of: blueValueLabel, from: blueSlider)
+        
+        setupSlider(redSlider, minimumTrackColor: .red)
+        setupSlider(greenSlider, minimumTrackColor: .green)
+        setupSlider(blueSlider, minimumTrackColor: .blue)
     }
     
     // MARK: IBActions
     @IBAction func moveSlider(_ sender: UISlider) {
+        var red = CGFloat(redSlider.value)
+        var green = CGFloat(greenSlider.value)
+        var blue = CGFloat(blueSlider.value)
+        
+        func setColor() {
+            finalColorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        }
+        
         switch sender.tag {
         case 0:
-            redValueLabel.text = String(format: "%.2f", sender.value)
+            red = CGFloat(sender.value)
+            setColor()
+            setLabelText(of: redValueLabel, from: sender)
         case 1:
-            greenValueLabel.text = String(format: "%.2f", sender.value)
+            green = CGFloat(sender.value)
+            setColor()
+            setLabelText(of: greenValueLabel, from: sender)
         case 2:
-            blueValueLabel.text = String(format: "%.2f", sender.value)
+            blue = CGFloat(sender.value)
+            setColor()
+            setLabelText(of: blueValueLabel, from: sender)
         default:
             print("Something gone wrong")
         }
     }
     
     // MARK: Private methods
-    private func connect(slider: UISlider, toLabel label: UILabel, trackColor color: UIColor) {
-        label.text = slider.value.formatted()
-        slider.minimumTrackTintColor = color
+    private func setupView(_ view: UIView) {
+        view.layer.cornerRadius = view.bounds.height * 0.15
+        view.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
     
-
+    private func setLabelText(of label: UILabel, from slider: UISlider) {
+        label.text = String(format: "%.2f", slider.value)
+    }
+    
+    private func setupSlider(_ slider: UISlider, minimumTrackColor color: UIColor) {
+        slider.minimumTrackTintColor = color
+        slider.maximumTrackTintColor = .placeholderText
+    }
 }
 
